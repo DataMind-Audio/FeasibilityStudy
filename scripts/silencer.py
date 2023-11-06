d8900e177ee98f14ea3bd2829668dd3af77739d3
Robin Leathart <56587304+rleathart@users.noreply.github.com>
2023-11-06 14:57:55 +0000

    Add a simplified silence remover script

---
+++ scripts/silencer.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import os, time
+import argparse
+import subprocess
+
+def main(args):
+    procs = []
+
+    log = subprocess.DEVNULL
+    if args.logfile:
+        log = open(args.logfile, "a")
+
+    main_start = time.time()
+    file_id = 0
+    for root, dirs, files in os.walk(args.input):
+        for file in files:
+            if file.lower().endswith(('.mp3', '.wav', '.flac', '.aiff', '.aif')):
+                in_path = os.path.join(root, file)
+                out_path = os.path.join(args.output, f"{file_id:05d}_{file}")
+                file_id += 1
+
+                print(f"{in_path} -> {out_path}")
+
+                ffmpeg_args = [
+                        'ffmpeg',
+                        '-i',
+                        in_path,
+                        '-af',
+                        f'silenceremove=stop_periods=-1:stop_duration={args.duration}:stop_threshold={args.threshold}dB',
+                        '-ac',
+                        '1',
+                        '-ar',
+                        '44100',
+                        '-c:a',
+                        'pcm_s16le',
+                        out_path,
+                        '-y'
+                ]
+
+                p = subprocess.Popen(ffmpeg_args, stdout=log, stderr=log)
+                procs.append(p)
+
+                max_procs = 32
+                while len(procs) >= max_procs:
+                    for p in procs:
+                        print(f"More than {max_procs} processes running (len(procs)), waiting for one to finish...")
+                        p.wait()
+                        break
+
+                for p in procs:
+                    if p.poll() is not None:
+                        print("Process {p} finished")
+                        procs.remove(p)
+
+    print(f"Waiting for {len(procs)} processes to finish...", end="")
+    for p in procs:
+        p.wait()
+
+    print(" done.")
+
+    main_end = time.time()
+
+    print(f"Processing took {main_end - main_start:.2f} seconds")
+
+    return
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', type=str, help='Input directory', required=True)
+    parser.add_argument('-o', '--output', type=str, help='Output directory', required=True)
+    parser.add_argument('-d', '--duration', type=float, help='Minimum silence duration in seconds', default=3.0)
+    parser.add_argument('-t', '--threshold', type=float, help='Threshold for silence in dB', default=-60.0)
+    parser.add_argument('--logfile', type=str, help='Path to log file', default=None)
+    args = parser.parse_args()
+
+    main(args)
